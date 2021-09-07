40be288a9327b5e09d392b2a346b4f3f0d6ee2d4
alan-wu <alan.wu@auckland.ac.nz>
2021-09-07 14:45:54 +1200

    Initial commit, got some sample pages going.

---
+++ layouts/error.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <breadcrumb-trail :breadcrumb="breadcrumb" :title="pageTitle" />
+    <div class="page-wrap container">
+      <div class="subpage">
+        <div>
+          <h4>
+            PAGE NOT FOUND
+          </h4>
+          <div>
+            <p>
+              I am sorry but we cannot find what you have been looking for.
+            </p>
+            <p>        
+              Maybe the page has moved - we suggest visiting the homepage to help you discover what you are looking for.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import graphcmsQuery from '@/services/graphcmsQuery'
+
+export default {
+  name: 'ErrorPage',
+
+  data: () => {
+    return {
+      pageTitle: 'PAGE NOT FOUND',
+      breadcrumb: [
+        {
+          to: {
+            name: 'index'
+          },
+          label: 'HOME'
+        }
+      ],
+    }
+  }
+}
+</script>
---
+++ services/graphcmsQuery.js
@@ -0,0 +1,24 @@
+import { gql } from 'graphql-request';
+
+async function content(graphcms, name) {
+  const variables = {
+    "name": name
+  }
+
+  const query = gql`
+    query ($name: String!) {
+      values: titledContent(where: {name: $name}) {
+        content {
+          html
+        }
+        title
+      }
+    }
+  ` 
+
+  return await graphcms.request(query, variables);
+}
+
+export default {
+  content
+}
