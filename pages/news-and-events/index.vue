cf005e9e4386c32d88bf3dbc42cf9c42b5f6789f
Riffat Nourin <rnou112@aucklanduni.ac.nz>
2021-10-06 11:24:49 +1300

    Implemented Banner component and updated news & events index page accordingly

--- pages/news-and-events/index.vue
+++ pages/news-and-events/index.vue
@@ -1,13 +1,9 @@
 <template>
   <div class="news-and-events-page">
     <breadcrumb-trail :breadcrumb="breadcrumb" :title="pageTitle.toUpperCase()" />
-    <div :style="urlStyle" class="news-events-banner">
-      <div class="banner__title-box">
-        <span>{{banner.values.title}}</span>
-      </div>
-    </div>
-    <latest-news :newsList="topNews.newsList"/>
-    <latest-events :eventsList="topEvents.eventsList"/>
+    <banner :banner-data="banner.values"/>
+    <latest-news :news-list="topNews.newsList"/>
+    <latest-events :events-list="topEvents.eventsList"/>
   </div>
 </template>
 
@@ -54,33 +50,5 @@ export default {
   .news-and-events-page{
     width:100%;
   }
-
-  .news-events-banner{
-    display:flex;
-    align-items:center;
-    justify-content:flex-end;    
-    height:300px;
-    background-image:var(--banner-url);
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-position: center;
-    overflow: hidden;
-  }
-
-  .banner__title-box{
-    height:108px;
-    width:340px;
-    margin-right:60px;
-    border-radius:20px;
-    background-color: $blue;
-    display:flex;
-    align-items:center;
-    justify-content:center;
-    span{
-      font-weight:bold;
-      font-size: 40px;
-      line-height: 44px;
-      color:$cochlear;
-    }
-  }
+  
 </style>
