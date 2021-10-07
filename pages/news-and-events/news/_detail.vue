36989145b37685acb74eaefe038ef566ae6de6f5
Riffat Nourin <rnou112@aucklanduni.ac.nz>
2021-10-08 12:35:39 +1300

    News List and detail pages

---
+++ pages/news-and-events/news/_detail.vue
@@ -0,0 +1,113 @@
+<template>
+    <div class="news-detail-page">
+        <breadcrumb-trail :breadcrumb="breadcrumb" :title="pageTitle.toUpperCase()"/>
+         <div class="detail-container">          
+            <img :src="newsItem.image.url" class="news-image"></img>         
+             <div class="news-title">
+                <h1>{{newsItem.title}}</h1>
+            </div>
+            <div class="date-social">
+                <div class="date-social__published">
+                    <span>Published {{newsItem.publishedDate}}</span>
+                </div>               
+                <social-box/>                  
+            </div>            
+            <div class="news-detail">
+                <span v-html="newsItem.detail.html">                      
+                </span>
+            </div>         
+            <social-box/>    
+            <div class="back-to-news">
+                <nuxt-link to="/news-and-events/news/">&lt; Back to News</nuxt-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'NewsItemDetail',
+
+    data: () => {
+        return {         
+            breadcrumb: [
+                {
+                to: { name: 'index'},
+                label: 'HOME'
+                },
+                {
+                to: {name: 'news-and-events'},
+                label: 'NEWS & EVENTS'
+                },
+                {
+                to: {name: 'news-and-events-news'},
+                label: 'NEWS'
+                }
+            ] 
+        }
+    },
+    
+    created() {
+        this.newsItem=this.$route.params.item,
+        this.pageTitle= this.newsItem.title
+    }
+}
+
+</script>
+
+
+<style scoped lang="scss">
+
+    .news-detail-page{
+        width:100%;   
+    }
+
+    .detail-container{
+        display:flex;
+        flex-direction:column;
+        margin:32px 310px;       
+    }
+
+    .news-image{
+        width: 100%;
+        height: 350px;
+        object-fit:cover;
+    }
+
+    .news-title{
+        margin-top:25px;
+        margin-bottom:10px;
+        h1{
+            margin:0px;
+        }
+    }
+
+    .date-social{
+        display:flex;
+        justify-content: space-between;
+        padding-bottom:6px;         //TBC - guess
+        &__published{
+            span{
+                font-size:14px;
+                line-height:20px;
+            }
+        }
+    }
+
+    .news-detail{
+        padding-top:32px;
+        padding-bottom:25px;        //TBC
+        border-top: 1px solid $lineColor1;
+        border-bottom: 1px solid $lineColor1;
+        margin-bottom:20px;     //TBC
+    }
+
+    .back-to-news{
+        padding-top:16px;
+        padding-bottom:8px;
+        a{
+            line-height:20px;
+        }
+    }
+</style>
