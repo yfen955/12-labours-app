f838ad2959807ddb27fc3199578e76b4c683924e
alan-wu <alan.wu@auckland.ac.nz>
2021-10-08 13:06:22 +1300

    Add images to the header and footer.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,31 @@
 <template>
   <div>
-    <twelve-labours-header />
+    <twelve-labours-header>
+      <template v-slot:logo>
+        <img class="header-logo" src="img/12-labours-logo-black.png" alt="Logo for 12 Labours">
+      </template>
+    </twelve-labours-header>
     <nuxt />
-    <twelve-labours-footer />
+    <twelve-labours-footer>
+      <template v-slot:logo>
+        <img class="footer-logo" src="img/12-labours-logo-primary.png" alt="Logo for 12 Labours">
+      </template>
+    </twelve-labours-footer>
   </div>
 </template>
+
+<style lang="scss" scoped>
+.header-logo {
+  display: block;
+  height: auto;
+  width: 100%;
+}
+.footer-logo
+{
+  height: 200px;   
+  width: 233px;  
+  white-space: nowrap;
+}
+
+</style>
+>>>>>>> Stashed changes
