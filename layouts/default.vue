69dedcac4607e5c7fb06f5678ada45b3111c85d2
alan-wu <alan.wu@auckland.ac.nz>
2021-10-22 10:23:19 +1300

    Replace footer links with some of the working ones.

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,13 +6,44 @@
       </template>
     </twelve-labours-header>
     <nuxt />
-    <twelve-labours-footer>
+    <twelve-labours-footer :footerLinks="footerLinks">
       <template v-slot:logo>
         <img class="footer-logo" src="img/12-labours-logo-primary.png" alt="Logo for 12 Labours">
       </template>
     </twelve-labours-footer>
   </div>
 </template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      footerLinks: [
+        {
+          title: "terms-of-service",
+          displayTitle: "Terms of Service",
+          href: "/terms-of-service"
+        },
+        {
+          title: "privacy-policy",
+          displayTitle: "Privacy Policy",
+          href: "/"
+        },
+        {
+          title: "site-feedback",
+          displayTitle: "Site Feedback",
+          href: "/site-feedback"
+        },
+        {
+          title: "contact",
+          displayTitle: "Contact",
+          href: "/contact-us"
+        }
+      ]
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .header-logo {
