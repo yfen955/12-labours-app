56569f23a73ec3bcb3337ed1abc8d802a813a83b
alan-wu <alan.wu@auckland.ac.nz>
2021-10-21 16:48:45 +1300

    Temporary direct data and models page to sparc.science/maps .

---
+++ pages/data/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Data And Models</h1>
+  </div>
+</template>
+<script>
+
+export default {
+
+  mounted() {
+    // Temporary redirect
+    if (window)
+      window.location.href = "https://sparc.science/maps";
+  },
+
+};
+</script>
