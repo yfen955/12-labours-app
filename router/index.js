13cbd11a1edd76bf09e5ae951dbffe1a1ab9b3cc
hsorby <h.sorby@auckland.ac.nz>
2021-10-22 18:13:36 +1300

    Update router to handle new pages.

--- router/index.js
+++ router/index.js
@@ -4,6 +4,48 @@ import { interopDefault } from "./utils";
 
 Vue.use(Router);
 
+const ContactUs = () =>
+  interopDefault(
+    import(
+      "../pages/contact-us/index.vue" /* webpackChunkName: "pages/contact-us/index" */
+    )
+  );
+const SiteFeecback = () =>
+  interopDefault(
+    import(
+      "../pages/site-feedback/index.vue" /* webpackChunkName: "pages/site-feedback/index" */
+    )
+  );
+const TermsOfService = () =>
+  interopDefault(
+    import(
+      "../pages/terms-of-service/index.vue" /* webpackChunkName: "pages/terms-of-service/index" */
+    )
+  );
+const NewsAndEventsEvents = () =>
+  interopDefault(
+    import(
+      "../pages/news-and-events/events/index.vue" /* webpackChunkName: "pages/news-and-events/events/index" */
+    )
+  );
+const NewsAndEventsNews = () =>
+  interopDefault(
+    import(
+      "../pages/news-and-events/news/index.vue" /* webpackChunkName: "pages/news-and-events/news/index" */
+    )
+  );
+const NewsAndEventsEventsDetail = () =>
+  interopDefault(
+    import(
+      "../pages/news-and-events/events/_detail.vue" /* webpackChunkName: "pages/news-and-events/events/_detail" */
+    )
+  );
+const NewsAndEventsNewsDetail = () =>
+  interopDefault(
+    import(
+      "../pages/news-and-events/news/_detail.vue" /* webpackChunkName: "pages/news-and-events/news/_detail" */
+    )
+  );
 const About = () =>
   interopDefault(
     import(
@@ -37,9 +79,44 @@ export function createRouter() {
         name: "about",
       },
       {
+        path: "/contact-us",
+        component: ContactUs,
+        name: "contact-us",
+      },
+      {
+        path: "/site-feedback",
+        component: SiteFeecback,
+        name: "site-feedback",
+      },
+      {
+        path: "/terms-of-service",
+        component: TermsOfService,
+        name: "terms-of-service",
+      },
+      {
         path: "/news-and-events",
         component: NewsAndEvents,
         name: "news-and-events",
+      },
+      {
+        path: "/news-and-events/events",
+        component: NewsAndEventsEvents,
+        name: "news-and-events-events",
+      },
+      {
+        path: "/news-and-events/news",
+        component: NewsAndEventsNews,
+        name: "news-and-events-news",
+      },
+      {
+        path: "/news-and-events/events/:detail?",
+        component: NewsAndEventsEventsDetail,
+        name: "news-and-events-events-detail",
+      },
+      {
+        path: "/news-and-events/news/:detail?",
+        component: NewsAndEventsNewsDetail,
+        name: "news-and-events-news-detail",
       },
       {
         path: "/resources/:pageName*",
