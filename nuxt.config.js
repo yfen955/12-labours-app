83f34e76a15a6856ccaa826a0968cc12b5cde2fe
Riffat Nourin <rnou112@aucklanduni.ac.nz>
2022-03-01 17:11:33 +1300

    Updates for consistent naming and paths for plugins' array

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,12 +43,12 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "@/plugins/dsc",
+    "@/plugins/dsc.js",
     "@/plugins/graphcms.js",
-    "@/plugins/helpers",
+    "@/plugins/helpers.js",
     "@/plugins/vue-sphinx-xml.js",
     "@/plugins/validators.js",
-    "@plugins/vue-gtag.client.js"
+    "@/plugins/vue-gtag.client.js"
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
