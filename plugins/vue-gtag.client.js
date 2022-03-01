190f6f744e4e77701d5857d45fa2f645f54b9925
Riffat Nourin <rnou112@aucklanduni.ac.nz>
2022-03-01 14:27:45 +1300

    Updates to pass router to implement g-tag

--- plugins/vue-gtag.client.js
+++ plugins/vue-gtag.client.js
@@ -2,6 +2,9 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
-Vue.use(VueGtag, {
-  config: { id: process.env.google_analytics_ga4 }
-})
+export default ({ app }) => {
+  Vue.use(VueGtag, {
+    config: { id: process.env.google_analytics_ga4 },
+    appName: '12-labours-app',
+  }, app.router)
+}
