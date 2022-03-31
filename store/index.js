bf7f335b06b34845ba69bd987932ccd1e2669313
Riffat Nourin <rnou112@aucklanduni.ac.nz>
2022-03-31 23:59:46 +1300

    Deleted unused code

--- store/index.js
+++ store/index.js
@@ -1,14 +1,12 @@
 export const state = () => ({
-  googleProfile:{}
+  
 });
 
 export const getters = {
-  getGoogleProfile: state => state.googleProfile
+  
 }
 
 
 export const mutations = {
-  setGoogleProfile(state,newProfile){
-    state.googleProfile=newProfile
-  }
+
 };
