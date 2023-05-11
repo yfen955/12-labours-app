94cb12adc0290b3aaa8f928c252d4f9ed9f969de
Frances Feng <arthur.f.kirkland.jones@gmail.com>
2023-05-11 16:17:27 +1200

    add build cmd & projectId

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "12-labours-app",
   e2e: {
     baseUrl: 'http://localhost:3000',
     // pageLoadTimeout: 1024*1024*1024,
