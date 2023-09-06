a5ac2ce7838e4c9df3e3ab181a129213e0073512
Frances Feng <arthur.f.kirkland.jones@gmail.com>
2023-09-06 14:59:36 +1200

    add 'view' in carousel buttons

--- components/CarouselCard2.vue
+++ components/CarouselCard2.vue
@@ -47,7 +47,7 @@
           </el-popover>
           <div class="card-button">
             <el-button @click="view(card.type, card.url, card.id)">
-              {{ card.type }}
+              View {{ card.type }}
             </el-button>
           </div>
         </el-card>
